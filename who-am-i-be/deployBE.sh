f1cd934bfdaed34523f84b1a583870a1a2e66c39
Mihai Anghelin <mihai.anghelin@gmail.com>
2023-04-11 02:21:47 +0300

    deploy script

---
+++ who-am-i-be/deployBE.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker build -t ghcr.io/mihaianghelin/who-am-i-be:master .
+docker push ghcr.io/mihaianghelin/who-am-i-be:master
+
+ssh mihui@192.168.100.35 "docker pull ghcr.io/mihaianghelin/who-am-i-be:master && docker compose -f /home/mihui/Docker/docker-compose.who-am-i.yaml up -d"
